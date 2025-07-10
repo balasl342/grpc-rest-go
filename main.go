1a029a9ff2675d5ef52777ca997a4dc0a4d93b20
Bala C <balasl342@gmail.com>
2025-07-10 16:56:58 +0530

    Initial commit to generate proto files for services, request and response

---
+++ main.go
@@ -0,0 +1,30 @@
+// main.go
+package main
+
+import (
+	pb "grpc-rest-go/hello"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	grpcAddr := ":50051"
+	restPort := ":8080"
+
+	// Start gRPC server
+	go func() {
+		lis, err := net.Listen("tcp", grpcAddr)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+		pb.RegisterHelloServiceServer(s, &helloServer{})
+		log.Printf("gRPC server listening on %s", grpcAddr)
+		s.Serve(lis)
+	}()
+
+	// Start REST gateway
+	startGateway(grpcAddr, restPort)
+}
